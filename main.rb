5805680a9d85e1b995d1f4ccc4eeba8255998478
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2022-05-03 15:57:56 -0700

    main: init label with foreground_color

--- main.rb
+++ main.rb
@@ -12,18 +12,19 @@ Data clumps - Extract class, Introduce whole object
 
 class Label 
 
-  attr_accessor :text, :x, :y 
+  attr_accessor :text, :x, :y, :foreground_color 
 
-  def initialize(text, x, y)
+  def initialize(text, x, y, foreground_color)
     @text = text
     @x = x
     @y = y
+    @foreground_color = foreground_color
   end
 end
 
 
 def draw_button(label_text, x, y, foreground_color, is_dark_mode)
-  label = Label.new(text, x, y)
+  label = Label.new(text, x, y, foreground_color)
   if is_dark_mode
     # darken foreground color for dark mode
     paint(label.text, label.x, label.y, foreground_color - 10, '#111111')
