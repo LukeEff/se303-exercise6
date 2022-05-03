3bb8a343c8b25f9a75fb92e2b122a0b9a3021ec6
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2022-05-03 15:55:25 -0700

    main: move field text

--- main.rb
+++ main.rb
@@ -25,9 +25,9 @@ def draw_button(label_text, x, y, foreground_color, is_dark_mode)
   label = Label.new(text, x)
   if is_dark_mode
     # darken foreground color for dark mode
-    paint(label_text, x, y, foreground_color - 10, '#111111')
+    paint(label.text, x, y, foreground_color - 10, '#111111')
   else
     # lighten foreground color for non-dark mode
-    paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+    paint(label.text, x, y, foreground_color + 10, '#E0E0E0')
   end
 end
